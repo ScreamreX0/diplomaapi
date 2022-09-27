72dcbdc92f88dbff8f7879d690c2f7e0896e3d25
Ruslanchik <scx0503@gmail.com>
2022-09-27 10:07:31 +0300

    Initial commit

---
+++ src/main/java/com/example/diplomaapi/DiplomaapiApplication.java
@@ -0,0 +1,12 @@
+package com.example.diplomaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomaapiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DiplomaapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/diplomaapi/DiplomaapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.diplomaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiplomaapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
